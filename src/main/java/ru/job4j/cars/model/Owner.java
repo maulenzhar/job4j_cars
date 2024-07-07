9a2705d3673ccc14c8c3e016ee48a5e58f68da1e
Maulen Zharylgasynov <exmpl@gmail.com>
2024-07-07 18:59:38 +0500

    Модели и связи. Машины и владельцы

--- src/main/java/ru/job4j/cars/model/Owner.java
+++ src/main/java/ru/job4j/cars/model/Owner.java
@@ -20,7 +20,8 @@ public class Owner {
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(name = "history_owner", joinColumns = {
-            @JoinColumn(name = "car_id", nullable = false, updatable = false)},
+            @JoinColumn(name = "car_id", nullable = false, updatable = false),
+            @JoinColumn(name = "history_id", nullable = false, updatable = false), },
             inverseJoinColumns = {
                     @JoinColumn(name = "owner_id", nullable = false, updatable = false)})
     private Set<Owner> owners = new HashSet<>();
--- src/main/resources/db/scripts/002_ddl_create_auto_post_schema.sql
+++ src/main/resources/db/scripts/002_ddl_create_auto_post_schema.sql
@@ -18,11 +18,19 @@ create table owner
     user_id int not null references auto_user (id)
 );
 
+create table history
+(
+    id      serial primary key,
+    start_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+    end_at   TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
+);
+
 create table history_owner
 (
     id       serial primary key,
     owner_id int not null references owner (id),
     car_id   int not null references car (id),
+    history_id int not null references history (id),
     UNIQUE (owner_id, car_id)
 );
 
@@ -41,12 +49,5 @@ create table auto_post
     created      TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     auto_user_id int references auto_user (id),
     car_id       int references car (id)
-);
-
-create table history
-(
-    id      serial primary key,
-    start_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
-    end_at   TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
 );
 
